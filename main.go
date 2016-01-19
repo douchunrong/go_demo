aad5d4694cf989fccf74a2cddda1126b8a12f13b
Traynor <traynordou@163.com>
2016-01-19 12:41:19 +0800

    a

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "go_demo/docs"
+	_ "go_demo/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
